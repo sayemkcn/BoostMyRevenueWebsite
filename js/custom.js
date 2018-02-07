bb46814b88faf1c89bce915303253ff6c0bc6998
Sayem Hossain <sayemkcn@gmail.com>
2018-02-08 03:22:45 +0600

    Changed language to english

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    // var accessToken = localStorage.getItem("B57UB5&i6u765&5b&*6");
+    // if(accessToken==null){
+    //     $("#notSignedInDropdown").show();
+    //     $("#signedInDropdown").hide();
+    // }else{
+    //     $("#notSignedInDropdown").hide();
+    //     $("#signedInDropdown").show();
+    // }
+});
